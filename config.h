4055c8ede91ad19f914eee1d9653f359111a5b82
jarbus <jarbus@tutanota.com>
2020-11-03 19:28:29 -0500

    latest working build

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int showsystray        = 1;
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int systrayspacing        = 2;
 static const int systraypinning        = 1;
 static const int systraypinningfailfirst        = 1;
@@ -18,17 +19,23 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 
 static const char *fonts[]          = { "monospace:size=18" };
 static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#070B11";
+static const char col_gray2[]       = "#070B11";
+static const char col_gray3[]       = "#9099a2";
+static const char col_gray4[]       = "#cfdbe8";
+static const char col_cyan[]        = "#3A5B8A";
+//static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+//#9099a2#9099a2
 
+static const char *const autostart[] = {
+	"sh","startup", NULL,
+	NULL /* terminate */
+};
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -37,9 +44,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
+	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -75,7 +85,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run","-m",dmenumon, NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "j4-dmenu-desktop", NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -112,17 +122,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+	//{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
   // custom commands
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("$HOME/.scripts/automonitor.sh") },
-	{ MODKEY,                       XK_p,      spawn,          SHCMD("$HOME/.scripts/lpass.sh") },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("zathura $(find /home/jack/drive/papers | dmenu -l 20 -F)") },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("automonitor") },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("qbp") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("papers") },
 	{ MODKEY,                       XK_e,      spawn,          SHCMD("dmenuunicode -1") },
 	{ MODKEY,                       XK_F12,    spawn,          SHCMD("light -A 5;kill -45 $(pidof dwmblocks)")},
 	{ MODKEY,                       XK_F11,    spawn,          SHCMD("light -U 5;kill -45 $(pidof dwmblocks)") },
 	{ MODKEY|ShiftMask,             XK_F11,    spawn,          SHCMD("light -S 0.1;kill -45 $(pidof dwmblocks)") },
 	{ MODKEY,                       XK_x,      spawn,          SHCMD("systemctl suspend") },
-	{ MODKEY,                       XK_c,      spawn,          SHCMD("gnome-screenshot -ic") },
 	{ MODKEY,                       XK_c,      spawn,          SHCMD("gnome-screenshot -ic") },
 	{ MODKEY,                       XK_equal,  spawn,          SHCMD("pulseaudio-ctl up; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY,                       XK_minus,  spawn,          SHCMD("pulseaudio-ctl down; kill -44 $(pidof dwmblocks)") },
