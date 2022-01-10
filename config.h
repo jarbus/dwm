243542687b3b81c54ffc8a8b018abc2d951c1261
jarbus <jarbus@tutanota.com>
2022-01-10 16:31:09 -0500

    working with kitty

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int showsystray        = 1;
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int systrayspacing        = 2;
 static const int systraypinning        = 1;
 static const int systraypinningfailfirst        = 1;
@@ -17,8 +17,8 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
-static const char *fonts[]          = { "JetBrainsMono:size=18" };
-static const char dmenufont[]       = "JetBrainsMono:size=15";
+static const char *fonts[]          = { "JetBrainsMono:size=24" };
+static const char dmenufont[]       = "JetBrainsMono:size=20";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#070B11";
 static const char col_gray3[]       = "#9099a2";
@@ -48,7 +48,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
-	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "kitty",      NULL,     NULL,           0,         0,          1,        0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
@@ -85,7 +85,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run","-m",dmenumon, NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "j4-dmenu-desktop", NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL}; //, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -103,7 +103,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_v,      spawn,      SHCMD("open-selection-in-vim") },
+	{ MODKEY,                       XK_t,      spawn,      SHCMD("en2es") },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,      SHCMD("es2en") },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -137,14 +139,16 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  spawn,          SHCMD("pulseaudio-ctl up; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY,                       XK_minus,  spawn,          SHCMD("pulseaudio-ctl down; kill -44 $(pidof dwmblocks)") },
   { 0, XF86XK_AudioMute,     		spawn,		SHCMD("pulseaudio-ctl mute; kill -44 $(pidof dwmblocks)") },
+
 	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pulseaudio-ctl up; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("pulseaudio-ctl down; kill -44 $(pidof dwmblocks)") },
 
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_s,      incrgaps,       {.i = +1 } },
+	{ MODKEY,                       XK_s,      spawn,          SHCMD("dicmenu") },
 	{ MODKEY|ShiftMask,             XK_s,      incrgaps,       {.i = -1 } },
 	{ MODKEY,                       XK_g,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_g,      spawn,          SHCMD("galaxy") },
 
 
 	{ MODKEY,                       XK_a,      spawn,          SHCMD("audioswitch;kill -44 $(pidof dwmblocks)") }
